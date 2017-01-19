75861c243c789b200bdc3812c0e8d49df511eab9
Andrew Lendacky <lendacka@ifarm1401.jlab.org>
2017-01-19 11:32:15 -0500

    Updated Bash Scripts

--- addPMTsToCCDB_1.sh
+++ addPMTsToCCDB_1.sh
@@ -3,6 +3,8 @@ cd ccdb
 source environment.csh
 cd ..
 
-for filename in *.txt; do
+  filename in *.txt; do
+
+  ccdb mktbl /test/rich/$filename -r 768 ver=double pmt=double  val=double
   ccdb add /test/rich/mapmts -v default $filename
 done
