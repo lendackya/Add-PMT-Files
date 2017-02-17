5a977be84b325d2ba75c592e6e9f09fc91a91a9b
Andrew Lendacky <lendacka@ifarm1401.jlab.org>
2017-02-17 11:06:27 -0500

    Changes to the Shell Script

--- addPMTsToCCDB_1.sh
+++ addPMTsToCCDB_1.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd ccdb
-source /group/clas12/environment.csh
-
-cd ..
-
 pwd
 cd data
 
@@ -13,9 +8,11 @@ for filename in *.txt; do
 	name=$(echo $filename | cut -f 1 -d '.')
 	echo "$filename"
 	echo "$name"
-	
-	#ccdb mktbl /test/rich/$name -r 768 ver=double pmt=double ntube=double runset=double rpos=double runt=double hv=double cls=double als=double adc1000=double adc1100=double cbsi=double gain=double pix=double yeild=double chi2=double nu2=double nu3=double a1=double a3=double nuav=double sc=double ssc=double sigma=double ssigma=double mu=double smu=double nu1=double snu1=double a2=double sa2=double pnu2=double spnu2=double pa3=double sa3=double pnu3=double spnu3=double xi=double sxi=double beta=double sbeta=double tau=double stau=double eff20=double eff25=double
-  	#ccdb add /test/rich/$name -v default $filename
+
+	tablepath="/test/rich/exp/"$name
+	echo "$tablepath"	
+	ccdb mktbl $tablepath -r 768 ver=double pmt=double ntube=double runset=double rpos=double runt=double hv=double cls=double als=double adc1000=double adc1100=double cbsi=double gain=double pix=double yeild=double chi2=double nu2=double nu3=double a1=double a3=double nuav=double sc=double ssc=double sigma=double ssigma=double mu=double smu=double nu1=double snu1=double a2=double sa2=double pnu2=double spnu2=double pa3=double sa3=double pnu3=double snu3=double xi=double sxi=double beta=double sbeta=double tau=double stau=double eff20=double eff25=double
+  	ccdb add $tablepath -v default $filename
 	
 done
 
